623e84a3319aed3b2c64c56f379d7dedc2c8d86c
RagenIV <z3-1415926@yandex.ru>
2017-04-29 09:44:00 +0400

    Update iot_secure_scanner.py

--- iot_secure_scanner.py
+++ iot_secure_scanner.py
@@ -4,6 +4,7 @@ import requests
 from subprocess import Popen, PIPE
 import random
 import string
+from urllib.parse import urlparse
 
 import my_modules
 
@@ -17,6 +18,13 @@ import crawler
 
 # Popen("ls -1", shell=True, stdin=PIPE, stdout=PIPE).stdout.read().split('\n')
 
+def get_root(url):
+	toor = urlparse(url)
+	if toor.netloc.find(':') == -1:
+		root = toor.netloc
+	else:
+		root = toor.netloc[:toor.netloc.find(':')]
+	return root
 
 def gen_pass(length):
 	ans = ''
@@ -27,28 +35,26 @@ def gen_pass(length):
 def send_mail(adr, rep):
 	ans = ''
 	for i in rep:
-		ans += i.replace('\n','\r\n') + '\r\n___________\r\n'
-	r = requests.get('http://scanner.juwilie.pw/sendMail.php?email=' + adr + '&text=' + ans')
-	print(ans)
+		ans += i + '\r\n___________\r\n'
+	r = requests.get('http://scanner.juwilie.pw/sendMail.php?email=' + adr + '&text=' + ans)
+
 
 def gogogo(dic):
-	r = requests.get('http://scanner.juwilie.pw/sendMail.php?email=19nick98@gmail.com&text='+dic['email']+'\r\n'+dic['host']+'\r\n'+str(dic['adv']))
+	# r = requests.get('http://scanner.juwilie.pw/sendMail.php?email=19nick98@gmail.com&text='+dic['email']+'\r\n'+dic['host']+'\r\n'+str(dic['adv']))
 	report = []
 	
 	report.append('Scanned device is ' + dic['host'])
-
+	root = get_root(dic['host'])
 	if dic['adv'] == '0':
-		'''
-		print('first_start')
-		report.append('Scan was intensive')
-		print('f1')
-		print(dic['host'])
-		webs = crawler.crawl(dic['host'], 13)
-		print('f2')
+		po = port_scaner.scanit(root,[22,23,25,80,443,1111,1234,3389,8000,8080,8888,9001])
+		if len(po) > 0:
+			report.append('Take care about these ports (they are open):\r\n'+'\r\n'.join(map(str, po)))
+		webs = crawler.crawl(str(dic['host']), 5)
 		for lin in webs:
 			rrr = requests_finder.findit(lin)
 			for r in rrr:
-				if ans[1] == 'post':
+				if r[1] == 'post':
+					print('epta')
 					logfi = 'etoneLoGin444'
 					pwdfi = 'eT0HEpassword'
 					for i in ans[2:]:
@@ -63,10 +69,11 @@ def gogogo(dic):
 						pwd = brute_post.brute(lin, logfi, pwdfi)
 						if len(pwd) == 2:
 							report.append("DANGER: your password isn't strong at all!!!\r\nSolution: change %s's password to more secure\r\nYou can use this: %s (generated automaticaly by random)" % (pwd[1], gen_pass(21)))
-		'''
-		pass
 	else:
 		report.append('Scan was wide')
+		po = port_scaner.scanit(root,[i for i in range(1235)] + [3389,7777,8000,8080,8888,9001])
+		if len(po) > 0:
+			report.append('Take care about these ports (they are open):\r\n'+'\r\n'.join(map(str, po)))
 		webs = crawler.crawl(str(dic['host']), 13)
 		
 		for lin in webs:
@@ -88,7 +95,16 @@ def gogogo(dic):
 						pwd = brute_post.brute(lin, logfi, pwdfi)
 						if len(pwd) == 2:
 							report.append("DANGER: your password isn't strong at all!!!\r\nSolution: change %s's password to more secure\r\nYou can use this: %s (generated automaticaly by random)" % (pwd[1], gen_pass(21)))
-		print('second_finish')
-	print('4321')
-	send_mail(dic['email'], report)
-	
+				elif r[1] == 'noget':
+					pass
+				else:
+					pass
+					# scan for get
+			
+			
+	try:
+		send_mail(dic['email'], report)
+		print('success')
+	except:
+		print('fiasco')
+
