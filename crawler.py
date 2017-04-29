30ea273e65ad13da68adf39131552ee7d6e684ba
RagenIV <z3-1415926@yandex.ru>
2017-04-29 08:16:21 +0400

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -4,22 +4,32 @@ import re
 import requests
 
 def microcrawl(url, links, deep2):
-	r = requests.get(url)
-	if r.status_code == 200:
-		con = r.text
-		f = re.findall('"(http\:\/\/.*?)"',con)
-		for lin in f:
-			if not(lin in links):
-				links.append(lin)
-		f = re.findall('"(https\:\/\/.*?)"',con)
-		for lin in f:
-			if not(lin in links):
-				links.append(lin)
-		f = re.findall('"(\/.*?)"',con)
-		for lin in f:
-			if not((url + lin) in links):
-				links.append(url + lin)
-	return deep2 - 1
+	dee = deep2 - 1
+	try:
+		r = requests.get(url)
+		if r.status_code == 200:
+			con = r.text
+			f = re.findall('"(http\:\/\/.*?)"',con)
+			for lin in f:
+				if root in f:
+					if not(lin in links):
+						links.append(lin)
+				else:
+					dee += 1
+			f = re.findall('"(https\:\/\/.*?)"',con)
+			for lin in f:
+				if root in f:
+					if not(lin in links):
+						links.append(lin)
+				else:
+					dee += 1
+			f = re.findall('"(\/.*?)"',con)
+			for lin in f:
+				if not((url + lin) in links):
+					links.append(url + lin)
+	except:
+		pass
+	return dee
 
 def crawl(url, deep):
 	deep2 = deep
@@ -38,15 +48,16 @@ def crawl(url, deep):
 	while 0 == 0:
 		newnew = []
 		for i in new:
-			if root in i:
-				if not(i in links):
-					links.append(i)
-					deep2 = microcrawl(i,newnew, deep2)
-					if deep2 < 0:
-						comeover = 0 == 0
-						break
+			if not(i in links):
+				links.append(i)
+				deep2 = microcrawl(i,newnew, deep2)
+				if deep2 < 0:
+					comeover = 0 == 0
+					break
 		if (len(newnew) == 0)or(comeover):
 			break
 		else:
 			new = newnew
 	return links
+
+# print(crawl('https://94.237.35.76:8443/spb/', 13))
